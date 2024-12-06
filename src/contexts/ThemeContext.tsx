730d1b30523b8fec7c8807f00e50fd3701b95e80
LeeTaeHeon <forever296@naver.com>
2024-12-06 18:53:39 +0900

    feat: 테마 상태 관리를 위한 컨텍스트 개발

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
+import { lightTheme, darkTheme } from '../styles/theme';
+
+interface ThemeContextProps {
+	isDarkMode: boolean;
+	toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const useTheme = () => {
+	const context = useContext(ThemeContext);
+	if (!context) {
+		throw new Error('useTheme must be used within a ThemeProvider');
+	}
+	return context;
+};
+
+export const CustomThemeProvider = ({ children }: { children: ReactNode }) => {
+	const [isDarkMode, setIsDarkMode] = useState(() => {
+		const savedTheme = localStorage.getItem('theme');
+		return savedTheme ? JSON.parse(savedTheme) : false;
+	});
+
+	const toggleTheme = () => {
+		setIsDarkMode((prevMode: boolean) => {
+			const newMode = !prevMode;
+			localStorage.setItem('theme', JSON.stringify(newMode));
+			return newMode;
+		});
+	};
+
+	return (
+		<ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+			<EmotionThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+				{children}
+			</EmotionThemeProvider>
+		</ThemeContext.Provider>
+	);
+};
