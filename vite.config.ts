d6d00603cc79c95806d26eee2bf5987d1f5bea5c
LeeTaeHeon <forever296@naver.com>
2025-01-07 18:09:44 +0900

    chore: react-query 관련 라이브러리 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { configDefaults } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [
@@ -10,6 +11,21 @@ export default defineConfig({
 			},
 		}),
 	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@mocks': path.resolve(__dirname, './src/mocks'),
+			'@stores': path.resolve(__dirname, './src/stores'),
+			'@apis': path.resolve(__dirname, './src/apis'),
+			'@hooks': path.resolve(__dirname, './src/hooks'),
+			'@utils': path.resolve(__dirname, './src/utils'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@styles': path.resolve(__dirname, './src/styles'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@constants': path.resolve(__dirname, './src/constants'),
+		},
+	},
 	test: {
 		globals: true,
 		environment: 'jsdom',
