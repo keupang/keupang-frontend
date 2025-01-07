87e5b2caefee489ee53953684a863f74ea688f61
LeeTaeHeon <forever296@naver.com>
2025-01-07 18:20:44 +0900

    remove: mock api 이메일 관련 코드 삭제

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,5 +1,5 @@
 // src/mocks/handlers.ts
-import { http, HttpResponse } from 'msw';
+import { http, HttpResponse, passthrough } from 'msw';
 interface BodyType {
 	email: string;
 	password: string;
@@ -38,40 +38,6 @@ export const handler = [
 				},
 			},
 			{ status: 201 }
-		);
-	}),
-
-	http.post('/api/auth/send-email', async ({ request }) => {
-		const { email } = (await request.json()) as BodyType;
-
-		verificationCode = Math.floor(100000 + Math.random() * 900000).toString(); // 6자리 랜덤 숫자
-
-		console.log(`발송한 이메일 주소 ${email}`);
-		console.log(`생성된 인증코드 ${verificationCode}`);
-
-		return HttpResponse.json(
-			{ message: '인증 이메일이 전송되었습니다.' },
-			{ status: 200 }
-		);
-	}),
-
-	http.post('/api/auth/verify-code', async ({ request }) => {
-		const { code } = (await request.json()) as BodyType;
-		if (!code) {
-			return HttpResponse.json(
-				{ message: '인증 코드를 입력해주세요.' },
-				{ status: 400 }
-			);
-		}
-		if (code !== verificationCode) {
-			return HttpResponse.json(
-				{ message: '인증 코드가 유효하지 않습니다.' },
-				{ status: 401 }
-			);
-		}
-		return HttpResponse.json(
-			{ message: '이메일 인증에 성공했습니다!' },
-			{ status: 200 }
 		);
 	}),
 ];
