ca44e635ef27b376b5b82791a8c307eb1e43b2fd
LeeTaeHeon <forever296@naver.com>
2025-01-08 17:22:41 +0900

    feat: 필요없는 mock api 삭제

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,14 +1,5 @@
 // src/mocks/handlers.ts
-import { http, HttpResponse, passthrough } from 'msw';
-interface BodyType {
-	email: string;
-	password: string;
-	name: string;
-	phone: string;
-	code: string;
-}
-
-let verificationCode: string | null = null;
+import { http, HttpResponse } from 'msw';
 
 export const handler = [
 	http.get('/api/products', () => {
@@ -16,28 +7,5 @@ export const handler = [
 			{ id: 1, name: 'Product A', price: 100 },
 			{ id: 2, name: 'Product B', price: 200 },
 		]);
-	}),
-
-	http.post('/api/users/register', async ({ request }) => {
-		const body = (await request.json()) as BodyType;
-
-		if (!body.email || !body.password || !body.name || !body.phone) {
-			return HttpResponse.json(
-				{ message: '필수 필드가 누락되었습니다.' },
-				{ status: 400 }
-			);
-		}
-
-		return HttpResponse.json(
-			{
-				message: '회원가입 성공!',
-				data: {
-					id: Math.floor(Math.random() * 1000),
-					email: body.email,
-					name: body.name,
-				},
-			},
-			{ status: 201 }
-		);
 	}),
 ];
