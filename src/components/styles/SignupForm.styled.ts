b4d510261dfc46aa5d5eda53239ffd8acfb7cf11
LeeTaeHeon <forever296@naver.com>
2025-01-09 16:50:23 +0900

    refactor: 자주 사용하는 스타일 모듈화에 따른 코드 수정

--- src/components/styles/SignupForm.styled.ts
+++ src/components/styles/SignupForm.styled.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { mediaQuery } from '../../utils/mediaQuery';
+import { Input } from '@/styles/commonStyles';
 
 export const FormContainer = styled.form`
 	display: flex;
@@ -8,11 +9,6 @@ export const FormContainer = styled.form`
 	justify-content: center;
 	padding: ${({ theme }) => theme.spacing.lg};
 	width: 100%;
-`;
-
-export const Title = styled.h1`
-	text-align: center;
-	margin-bottom: ${({ theme }) => theme.spacing.md};
 `;
 
 export const InputWrapper = styled.div`
@@ -26,38 +22,6 @@ export const InputWrapper = styled.div`
 	${mediaQuery('md')} {
 		width: 100%;
 	}
-`;
-
-export const Input = styled.input`
-	width: 100%;
-	padding: ${({ theme }) => theme.spacing.sm};
-	border: 1px solid #ffffff;
-	border-radius: 4px;
-	font-size: ${({ theme }) => theme.fontSizes.md};
-	background-color: #6b7280;
-	color: #ffffff;
-	display: flex;
-
-	&::placeholder {
-		color: #cccccc;
-	}
-	&:focus {
-		outline: none;
-		border-color: ${({ theme }) => theme.colors.primary};
-	}
-
-	&:disabled {
-		background-color: #3e3e3e;
-		color: #a1a1a1;
-		border-color: #555555;
-		cursor: not-allowed;
-	}
-`;
-
-export const ErrorText = styled.p`
-	color: ${({ theme }) => theme.colors.danger};
-	font-size: ${({ theme }) => theme.fontSizes.sm};
-	margin: 0 0 ${({ theme }) => theme.spacing.sm};
 `;
 
 export const Notice = styled.p`
