083219ef3392a41eb7606310ea8a6b2f14a3513e
LeeTaeHeon <forever296@naver.com>
2025-03-27 17:21:11 +0900

    chore: 메인페이지에 반영될 개발자 정보 입력

---
+++ src/constants/profile.ts
@@ -0,0 +1,34 @@
+import taeheonPhoto from '@/assets/images/taeheon.webp';
+import jonghakPhoto from '@/assets/images/jonghak.webp';
+
+export const PROFILES = [
+	{
+		id: 'forever296@naver.com',
+		name: '이태헌',
+		photo: taeheonPhoto,
+		field: 'Frontend',
+		github: 'https://github.com/forever2969',
+		blog: 'https://velog.io/@forever296/posts',
+		experience: [
+			'Typecript + React Keupang 프로젝트 1인 개발',
+			'멋쟁이사자처럼 10기 아기사자 중앙 해커톤 Top 10',
+			'멋쟁이사자처럼 11기 Frontend Track Leader',
+			'F-Lab 프론트엔드 분야 수료',
+			'한국정보기술원 대학생논문대회 은상 🥈',
+		],
+	},
+	{
+		id: 'cj855695@gmail.com',
+		name: '최종학',
+		photo: jonghakPhoto,
+		field: 'Backend',
+		github: 'https://github.com/JongHak19',
+		blog: 'https://back-jong.tistory.com',
+		experience: [
+			'Spring Boot + MSA구조 Keupang 프로젝트 1인 개발',
+			'Grand ICT 연구 센터 - MES 시스템 유지보수 경험(JSP, Java)',
+			'GDSC-DEU 동아리 활동',
+			'F-Lab 백엔드 분야 수료',
+		],
+	},
+];
