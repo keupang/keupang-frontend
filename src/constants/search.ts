e35fef53666ce0d94339050e12de9d788e9894c6
LeeTaeHeon <forever296@naver.com>
2025-03-28 17:23:43 +0900

    chore: searchFilter 상수 작성

--- src/constants/search.ts
+++ src/constants/search.ts
@@ -2,5 +2,12 @@ export const searchDefault = {
 	size: '20',
 	page: '0',
 	category: '',
-	query: '',
+	search: '',
+};
+
+export const searchFilter = {
+	최신순: 'new',
+	판매량순: 'sales',
+	'낮은 가격순': 'low',
+	'높은 가격순': 'high',
 };
